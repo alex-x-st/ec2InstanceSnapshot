03a687db078ec746ae927815ed6ac06b9fca1966
alex-x-st <aleksey.v.scherbakov@gmail.com>
2018-10-27 14:10:59 -0700

    Snapshot done

--- ec2InstanceSnapshots.ps1
+++ ec2InstanceSnapshots.ps1
@@ -5,12 +5,25 @@ function New-EC2InstanceSnapshots {
         [String]$InstanceID
     )
 
-    # Get instance    
+    # Get instance
+    $Instance = Get-EC2Instance -InstanceId $InstanceID
+    #$Instance.instances | Select *
+
     # Get all volumes per inctance (array)
-    # New snapshot for each volume of the instance 
+    $Volumes = @(get-ec2volume) | ? { $_.Attachments.InstanceId -eq $InstanceID}
+    #forEach ($Volume in $Volumes) {
+    #    $volume | Select *
+    #}
     
+    # New snapshot for each volume of the instance
+    forEach ($Volume in $Volumes) {
+        New-EC2Snapshot -VolumeId $Volume.VolumeId -Description "Automated snapshot - New-EC2InstanceSnapshots"
+    }    
 }
 
 
 Import-Module AWSPowerShell
 
+$InstanceId = "i-0afb5a4f3feb27c4a"
+
+New-EC2InstanceSnapshots -InstanceID $InstanceId 
