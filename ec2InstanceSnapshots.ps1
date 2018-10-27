a41a99f8d555e436b8758a9a695b30606ff6d221
alex-x-st <aleksey.v.scherbakov@gmail.com>
2018-10-27 13:27:35 -0700

    add *

---
+++ ec2InstanceSnapshots.ps1
@@ -0,0 +1,16 @@
+﻿
+
+function New-EC2InstanceSnapshots {
+    param (
+        [String]$InstanceID
+    )
+
+    # Get instance    
+    # Get all volumes per inctance (array)
+    # New snapshot for each volume of the instance 
+    
+}
+
+
+Import-Module AWSPowerShell
+
